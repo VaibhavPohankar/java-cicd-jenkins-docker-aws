8ce228f23d61314726939777c6785e37f6abfe5a
vaibhavpohankar <vaibhavpohankar30@gmail.com>
2026-03-29 17:28:58 +0530

    Change server port from 8080 to 9090

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -15,7 +15,7 @@ public class App {
 
     public static void main(String[] args) throws Exception {
 
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        HttpServer server = HttpServer.create(new InetSocketAddress(9090), 0);
 
         server.createContext("/", exchange -> {
             exchange.sendResponseHeaders(200, MESSAGE.length());
@@ -25,6 +25,6 @@ public class App {
         });
 
         server.start();
-        System.out.println("Server started on port 8081");
+        System.out.println("Server started on port 9090");
     }
 }
